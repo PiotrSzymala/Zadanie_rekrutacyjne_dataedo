7a6933b52271705cfb6c3bcae8a02286221da9f0
Piotr Szymala <p.szymala@sellpander.pl>
2024-06-13 18:03:28 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var reader = new Parser();
+            reader.Do("sampleFile1.csv", "dataSource.csv");
+        }
+    }
+}
