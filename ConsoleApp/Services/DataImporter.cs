b60adadb1afcaa31f9ac139e24dd3396a228aec3
Piotr Szymala <p.szymala@sellpander.pl>
2024-06-17 19:13:03 +0200

    Refactor DataImporter class.

--- ConsoleApp/Services/DataImporter.cs
+++ ConsoleApp/Services/DataImporter.cs
@@ -8,46 +8,70 @@ using ConsoleApp.Interfaces;
 
 namespace ConsoleApp.Services
 {
-    internal class DataImporter : IDataImporter
+    public class DataImporter : IDataImporter
     {
         public IList<ImportedObject> Import(string filePath)
         {
-           var importedObjects = new List<ImportedObject>();
-           using (var streamReader = new StreamReader(filePath))
-           {
-               string line;
+            var importedObjects = new List<ImportedObject>();
+            using (var streamReader = new StreamReader(filePath))
+            {
 
-               while ((line = streamReader.ReadLine()) != null )
-               {
-                   importedObjects.Add(ParseLine(line));
-               }
-           }
+                string headerLine = streamReader.ReadLine();
 
-           return importedObjects;
+                var headerMap = ParseHeaders(headerLine);
+
+                string line;
+
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    var values = line.Split(';');
+                    var importedObject = ParseLine(values, headerMap);
+                    importedObjects.Add(importedObject);
+                }
+            }
+
+            return importedObjects;
         }
 
-        private ImportedObject ParseLine(string line)
+        private static Dictionary<string, int> ParseHeaders(string headerLine)
         {
-            var values = line.Split(';');
+            return headerLine.Split(';')
+                .Select((header, index) => new { Header = header, Index = index })
+                .ToDictionary(h => h.Header, h => h.Index);
+        }
+
+        private static ImportedObject ParseLine(IReadOnlyList<string> values,
+            IReadOnlyDictionary<string, int> headerMap)
+        {
             return new ImportedObject
             {
-                Type = GetValueSafely(values, 0),
-                Name = GetValueSafely(values, 1),
-                Schema = GetValueSafely(values, 2),
-                ParentName = GetValueSafely(values, 3),
-                ParentType = GetValueSafely(values, 4),
-                ParentSchema = GetValueSafely(values, 5),
-                Title = GetValueSafely(values, 6),
-                Description = GetValueSafely(values, 7),
-                CustomField1 = GetValueSafely(values, 8),
-                CustomField2 = GetValueSafely(values, 9),
-                CustomField3 = GetValueSafely(values, 10)
+                Type = GetValueSafely(values, GetIndexSafely(headerMap, "Type")),
+                Name = GetValueSafely(values, GetIndexSafely(headerMap, "Name")),
+                Schema = GetValueSafely(values, GetIndexSafely(headerMap, "Schema")),
+                ParentName = GetValueSafely(values, GetIndexSafely(headerMap, "ParentName")),
+                ParentType = GetValueSafely(values, GetIndexSafely(headerMap, "ParentType")),
+                ParentSchema = GetValueSafely(values, GetIndexSafely(headerMap, "ParentSchema")),
+                Title = GetValueSafely(values, GetIndexSafely(headerMap, "Title")),
+                Description = GetValueSafely(values, GetIndexSafely(headerMap, "Description")),
+                CustomField1 = GetValueSafely(values, GetIndexSafely(headerMap, "CustomField1")),
+                CustomField2 = GetValueSafely(values, GetIndexSafely(headerMap, "CustomField2")),
+                CustomField3 = GetValueSafely(values, GetIndexSafely(headerMap, "CustomField3")),
             };
         }
 
-        private string GetValueSafely(string[] values, int index)
+        private static int GetIndexSafely(IReadOnlyDictionary<string, int> map, string key)
         {
-            return index < values.Length ? values[index] : null;
+            if (map.TryGetValue(key, out var safely))
+            {
+                return safely;
+            }
+
+            return -1;
+        }
+
+        private static string GetValueSafely(IReadOnlyList<string> values, int index)
+        {
+            return index >= 0 && index < values.Count ? values[index].Clear() : "";
         }
     }
 }
