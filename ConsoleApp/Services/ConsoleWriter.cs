ee128ad703ea0b648035cb97c717dd21a7434a31
Piotr Szymala <p.szymala@sellpander.pl>
2024-06-18 19:11:17 +0200

    Provide testing possibility for DataPrinter.

---
+++ ConsoleApp/Services/ConsoleWriter.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Services
+{
+    public class ConsoleWriter : IConsoleWriter
+    {
+        public void WriteLine(string message) => Console.WriteLine(message);
+        public void SetForegroundColor(ConsoleColor color) => Console.ForegroundColor = color;
+        public void ResetColor() => Console.ResetColor();
+    }
+}
